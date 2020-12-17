ab2c412bf5b2e5e8513c6722855518fa76834690
admin <455727830@qq.com>
2020-12-17 15:37:26 +0800

    new router

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Day from '../views/day-view.vue'
+// import Week from '../views/week-view.vue'
+//import Month from '../views/month-view.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'day-view',
+    component: Day
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/week',
+    name: 'day-view',    
+    component: () => import(/* webpackChunkName: "about" */ '../views/week-view.vue')
+  },
+  {
+    path:'/month',
+    name:'month-view',
+    component:()=>import('../views/month-view.vue')
   }
 ]
 
